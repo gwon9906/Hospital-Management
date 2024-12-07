69b86045c446640f21ab2c961d0857bc1de8430b
gwon9 <gwon99065@naver.com>
2024-12-07 22:23:58 +0900

    오류 수정

--- src/main/java/com/hospital/management/hospitalmanagement/controller/MainController.java
+++ src/main/java/com/hospital/management/hospitalmanagement/controller/MainController.java
@@ -1,6 +1,7 @@
 package com.hospital.management.hospitalmanagement.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
